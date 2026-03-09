909279451ec5ac58618b689c0deb898e12dc3901
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-03-09 09:13:45 -0700

    Remove Hellow World test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,29 +15,27 @@ import NotFound from './pages/NotFound.jsx';
 import EditMealTrain from './pages/EditMealTrain.jsx';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Routes>
+      <Route element={<GuestRoutes />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+
+      <Route path="/" element={<Home />} />
+
+      <Route element={<PrivateRoutes />}>
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/create-meal-train" element={<CreateMealTrain />} />
+        <Route path="/single-meal-view" element={<SingleMealView />} />
+        <Route path="/create-meal/:id" element={<CreateMeal />} />
+        <Route path="/view-meal-train/:id" element={<ViewMealTrain />} />
+        <Route path="/edit-meal-train/:id" element={<EditMealTrain />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
-//   return (
-//     <Routes>
-//       <Route element={<GuestRoutes />}>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//       </Route>
-
-//       <Route path="/" element={<Home />} />
-
-//       <Route element={<PrivateRoutes />}>
-//         <Route path="/dashboard" element={<UserDashboard />} />
-//         <Route path="/create-meal-train" element={<CreateMealTrain />} />
-//         <Route path="/single-meal-view" element={<SingleMealView />} />
-//         <Route path="/create-meal/:id" element={<CreateMeal />} />
-//         <Route path="/view-meal-train/:id" element={<ViewMealTrain />} />
-//         <Route path="/edit-meal-train/:id" element={<EditMealTrain />} />
-//       </Route>
-
-//       <Route path="*" element={<NotFound />} />
-//     </Routes>
-//   );
-// }
 
 export default App;
